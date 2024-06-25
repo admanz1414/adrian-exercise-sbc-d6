ed6b216295e29cb6027e17ae330a05bc6d889d01
admanz1414 <admanz0301@gmail.com>
2024-06-24 19:43:53 -0700

    Add files via upload

---
+++ adrian3.py
@@ -0,0 +1,52 @@
+"""""
+n =0
+
+while n <=5:
+     print(n)
+
+     n += 1
+
+li = [1, 2, 3, 4,]
+
+for item  in li:
+     print(item)
+
+string = " hi world"
+
+for char in string:
+     print(1)
+
+n1_= ["adrian", "mandras", "guibone",]
+
+for name in n1_:
+     print(name)
+
+for n in range(1, 10, 2):
+     print(n)
+"""""
+"""""
+while loop
+
+while True
+print("x")
+
+"""
+"""""
+n = 0
+while n <18:
+    n1 = int(input("how old are you"))
+    print(n1)
+    
+else:
+    print("aydolll tikaaa")
+"""
+
+log = True
+while log:
+    username = input("username: ")
+    password = input("password: ")
+    if username == "adrian" and password == "adrian123":
+        print("Succescfully login")
+     
+    else:
+        print("login failed try Again")
